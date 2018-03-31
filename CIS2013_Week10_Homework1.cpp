67fbb2ff370c603d191bff3071e2771de96b3415
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-31 17:12:52 -0500

    board print added

--- CIS2013_Week10_Homework1.cpp
+++ CIS2013_Week10_Homework1.cpp
@@ -8,6 +8,10 @@ int main(){
 int w;
 int h;
 int bombs;
+CharPtr x;
+CharPtr y;
+
+// void print_board(int, int, int);
 
 	cout << "Enter width: ";
 	cin >> w;
@@ -16,5 +20,20 @@ int bombs;
 	cout << "Enter number of bombs: ";
 	cin >> bombs;
 	
+	x = new char[w];
+	y = new char[h];
+	for (int n=0; n<=h; n++){
+	for (int i=0; i<=w; i++){
+		x[i] = 'X';
+		cout << " " << x[i] << " ";
+	}
+	cout << endl;
+}
 	return 0;
 }
+
+// void print_board (int w, int h, int b){
+	// for (int i=0; i<=w; i++){
+		
+	// }
+// }
