927f495f2ead6dc3f8be5b930361442cb155e1c4
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-31 16:56:06 -0500

    starting questions

---
+++ CIS2013_Week10_Homework1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+typedef char* CharPtr;
+
+int main(){
+
+int w;
+int h;
+int bombs;
+
+	cout << "Enter width: ";
+	cin >> w;
+	cout << "Enter height: ";
+	cin >> h;
+	cout << "Enter number of bombs: ";
+	cin >> bombs;
+	
+	return 0;
+}
